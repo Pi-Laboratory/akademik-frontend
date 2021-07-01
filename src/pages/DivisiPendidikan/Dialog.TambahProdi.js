09b5c7388128f3528d9dfedc5e276b3f486940d1
fransiscotarut-hub <shishuko27@gmail.com>
2021-07-01 10:03:12 +0800

    + Pemilihan Jurusan di Tambah Prodi

--- src/pages/DivisiPendidikan/Dialog.TambahProdi.js
+++ src/pages/DivisiPendidikan/Dialog.TambahProdi.js
@@ -38,7 +38,25 @@ const DialogMahasiswaBaru = ({ isOpen, onClose = () => { } }) => {
                   intent={errors["nama prodi"] ? "danger" : "none"}
                 />
               </FormGroup>
-              
+              <FormGroup
+                label="Jurusan"
+                labelFor="jurusan"
+                helperText={errors["jurusan"]}
+                intent={"danger"}
+              >
+                <div class="bp3-select .modifier">
+                  <select>
+                    <option selected>-- PILIH ---</option>
+                    <option value="1">Teknik Sipil</option>
+                    <option value="2">Teknik Mesin</option>
+                    <option value="3">Teknik Elektro</option>
+                    <option value="4">Akademi Pariwisata</option>
+                    <option value="5">Akuntansi</option>
+                    <option value="6">Administrasi Bisnis</option>
+                    
+                  </select>
+                </div>
+              </FormGroup>
             </div>
             <div className={Classes.DIALOG_FOOTER}>
               <div className={Classes.DIALOG_FOOTER_ACTIONS}>
