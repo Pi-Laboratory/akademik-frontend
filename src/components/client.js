80adffe1637b47048924ccf8328d4df5cc39ef7b
Imanuel Pundoko <ilomon10@gmail.com>
2021-08-21 14:21:42 +0800

    Konfigurasi adapter client

--- src/components/client.js
+++ src/components/client.js
@@ -1,23 +1,47 @@
-import SiriusAdapter from "@edgarjeremy/sirius.adapter";
-import { createContext, useContext, useEffect, useMemo, useRef, useState } from "react";
+import Feathers from "@feathersjs/feathers";
+import FeathersAuth from "@feathersjs/authentication-client";
+import FeathersSocketIOClient from "@feathersjs/socketio-client";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import io from "socket.io-client";
 
-const host = process.env["REACT_APP_SERVER_HOST"];
-const port = process.env["REACT_APP_SERVER_PORT"];
 
-const adapter = new SiriusAdapter(host, port, localStorage);
+const host = new URL(window.location.origin);
+host.hostname = process.env["REACT_APP_SERVER_HOST"];
+host.port = process.env["REACT_APP_SERVER_PORT"];
+
+const socket = io(host.toString());
+
+const feathers = Feathers();
+feathers.configure(FeathersSocketIOClient(socket))
+feathers.configure(FeathersAuth({
+  storageKey: "accessToken"
+}))
 
 export const ClientContext = createContext(null);
 
 export const ClientProvider = ({ children }) => {
+  const [account, setAccount] = useState(null);
   const [isConnected, setIsConnected] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const factories = useRef(null);
-  const authentication = useRef(adapter.getAuthProvider());
 
-  async function authenticate(data) {
+  useEffect(() => {
+    const listener = [
+      () => { setIsConnected(true); },
+      () => { setIsConnected(false); }
+    ]
+    socket.on("connect", listener[0]);
+    socket.on("disconnect", listener[1]);
+    return () => {
+      socket.off("connect", listener[0]);
+      socket.off("disconnect", listener[1]);
+    }
+  }, []);
+
+  async function authenticate(data, params) {
     try {
-      const res = await authentication.current.set(data);
+      const res = await feathers.authenticate(data, params);
       console.log(res);
+      setAccount(res.users);
       setIsAuthenticated(true);
     } catch (err) {
       throw new Error(err);
@@ -26,26 +50,35 @@ export const ClientProvider = ({ children }) => {
 
   const client = useMemo(() => {
     return {
-      adapter,
+      feathers,
+      account,
+
       authenticate,
-      authentication: authentication.current,
-      factories: factories.current,
+      logout: () => { return feathers.logout() },
+      reAuthenticate: (force) => { return feathers.reAuthenticate(force) },
+
       isAuthenticated() { return isAuthenticated },
       isConnected() { return isConnected },
       __connected: isConnected,
       __authenticated: isAuthenticated,
-    }
-  }, [isConnected, isAuthenticated]);
 
-  useEffect(() => {
-    if (isConnected) return;
-    const fetch = async () => {
-      const f = await client.adapter.connect();
-      factories.current = f;
-      setIsConnected(true);
+      get: (name) => { return feathers.get(name) },
+
+      // Services
+      "users": () => { return feathers.service("users") },
+      "rooms": () => { return feathers.service("rooms") },
+      "semesters": () => { return feathers.service("semesters") },
+      "students": () => { return feathers.service("students") },
+      "lecturers": () => { return feathers.service("lecturers") },
+      "subjects": () => { return feathers.service("subjects") },
+      "majors": () => { return feathers.service("majors") },
+      "study-programs": () => { return feathers.service("study-programs") },
+      "schedules": () => { return feathers.service("schedules") },
+      "hours": () => { return feathers.service("hours") },
+      "classes": () => { return feathers.service("classes") },
+      "curriculums": () => { return feathers.service("curriculums") },
     }
-    fetch();
-  }, [client, isConnected]);
+  }, [isConnected, isAuthenticated, account]);
 
   return (
     <ClientContext.Provider value={client}>
