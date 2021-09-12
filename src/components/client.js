0ff1d2336240dc8742058853e8becc80944ca757
Imanuel Pundoko <ilomon10@gmail.com>
2021-09-13 00:41:11 +0800

    Memperbaiki env variable koneksi client

--- src/components/client.js
+++ src/components/client.js
@@ -6,6 +6,7 @@ import io from "socket.io-client";
 
 
 const host = new URL(window.location.origin);
+host.protocol = process.env["REACT_APP_SERVER_PROTOCOL"];
 host.hostname = process.env["REACT_APP_SERVER_HOST"];
 host.port = process.env["REACT_APP_SERVER_PORT"];
 
