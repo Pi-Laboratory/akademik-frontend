24de619db6b92a66f8318df189bf5957edca6c03
Imanuel Pundoko <ilomon10@gmail.com>
2021-09-25 00:45:18 +0800

    Perbaiki halaman detail pewagai

--- src/pages/Pegawai.Details/Layout.js
+++ src/pages/Pegawai.Details/Layout.js
@@ -30,7 +30,7 @@ const Layout = () => {
               {[
                 ["NIP", item["nip"]],
                 ["NIDN", item["nidn"]],
-                ["Nama Lengkap", `${item["front_degree"]}${item["name"]}${item["back_degree"]}`],
+                ["Nama Lengkap", `${item["front_degree"] || ""}${item["name"]}${item["back_degree"] || ""}`],
                 ["NIK", item["nik"]],
                 ["Tempat Lahir", item["birth_city"]],
                 ["Tanggal Lahir", item["birth_date"]],
@@ -42,8 +42,7 @@ const Layout = () => {
                 ["Negara", item["country"]],
                 ["Kabupaten/Kota", item["city"]],
                 ["Nomor Telepon", item["phone_number"]],
-                ["Email", item["email"]],
-                ["Jenis Pegawai", (["Pengajar", "Staff"])[item["status"]]],
+                ["Email", item["email"]]
               ].map((value, idx) => (
                 <tr>
                   <td>
@@ -54,20 +53,6 @@ const Layout = () => {
               ))}
             </tbody>
           </HTMLTable>}
-        <Box as={H3} sx={{ mt: 4 }}>Informasi Pejabat</Box>
-        {[
-          ["Jabatan", "-"],
-          ["Program Studi", "-"],
-        ].map((item, idx) => (
-          <Flex key={idx} sx={{ mt: 3 }}>
-            <Box sx={{ width: "40%", flexShrink: 0, fontWeight: "bold", color: "gray.6" }}>
-              <span>{item[0]}</span>
-            </Box>
-            <Box sx={{ flexGrow: 1 }}>
-              <span>{item[1]}</span>
-            </Box>
-          </Flex>
-        ))}
       </Box>
       <Divider vertical={true} />
       <Box sx={{ pt: 4, px: 2, width: 350, flexShrink: 0 }}>
@@ -81,10 +66,10 @@ const Layout = () => {
           </AspectRatio>
         </Box>
         <Box sx={{ fontSize: 3, mb: 2 }}>
-          ADRIYAN WAROKKA, ST.,M.ENG
+          {item && `${item["front_degree"] || ""}${item["name"]}${item["back_degree"] || ""}`}
         </Box>
         <Box sx={{ fontWeight: "bold", color: "gray.6" }}>
-          197807102008121003
+          {item && item["nip"]}
         </Box>
       </Box>
     </Flex>
