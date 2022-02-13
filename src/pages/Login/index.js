2c6cb0e08e81d4fb036a1a9d2a0bd84fec94e978
Imanuel Pundoko <ilomon10@gmail.com>
2022-02-13 15:32:45 +0800

    Update registration method on Login page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,6 +3,7 @@ import { Box, Flex } from "components";
 import { useState } from "react";
 import Email from "./Email";
 import { host } from "components/client";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [logType, setLogType] = useState(null);
@@ -11,8 +12,16 @@ const Login = () => {
     <Box sx={{
       width: 275,
       mx: "auto",
-      mt: 4,
+      pt: 4,
     }}>
+      <Box sx={{ textAlign: "center", py: 3 }}>
+        <Box as="h1" sx={{
+          fontSize: 4,
+          fontWeight: "lighter"
+        }}>
+          Masuk ke Akademik
+        </Box>
+      </Box>
       <Box sx={{
         borderRadius: 4,
         border: "1px solid white",
@@ -30,7 +39,7 @@ const Login = () => {
           {logType === null &&
             <Box>
               <Box sx={{ px: 3, mb: 3, textAlign: "center" }}>
-                Pilih cara login Anda
+                Pilih cara masuk Anda
               </Box>
               <Flex sx={{
                 flexDirection: "column",
@@ -64,6 +73,25 @@ const Login = () => {
             </Box>
           }
         </Box>
+      </Box>
+      <Box sx={{
+        mt: 3,
+        p: 3,
+        borderRadius: 4,
+        border: "1px solid white",
+        borderColor: "gray.3",
+        textAlign: "center"
+      }}>
+        <Box as="span">
+          Mahasiswa baru? Registrasi <Link to="/registrasi">disini</Link>.
+        </Box>
+      </Box>
+      <Box sx={{
+        textAlign: "center",
+        my: 4,
+        fontSize: 0,
+      }}>
+        <a href="mailto:ilomon10@gmail.com">Laporkan sesuatu</a>
       </Box>
     </Box>
   )
