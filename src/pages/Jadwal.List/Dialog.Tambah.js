1963ce840ef190b2f3a2d14fe881bab67a33cca2
Imanuel Pundoko <ilomon10@gmail.com>
2022-03-24 17:07:31 +0800

    Memperbaiki pesan validasi pada jadwal

--- src/pages/Jadwal.List/Dialog.Tambah.js
+++ src/pages/Jadwal.List/Dialog.Tambah.js
@@ -5,12 +5,13 @@ import { FieldArray, Formik } from "formik";
 import moment from "moment";
 import { useCallback, useState } from "react";
 import * as Yup from "yup";
+import _get from "lodash/get";
 
 const Schema = Yup.object().shape({
   "subject_id": Yup.number().required(),
   "lecturer_id": Yup.number().required(),
   "hours": Yup.array().of(Yup.object().shape({
-    "day": Yup.number(),
+    "day": Yup.number().typeError('required'),
     "start": Yup.date().default(() => {
       return new Date();
     }),
@@ -38,10 +39,13 @@ const DialogTambah = ({
     lecturers: false,
   });
 
-  const fetchSubjects = useCallback(async () => {
+  const fetchSubjects = useCallback(async (query) => {
     setLoading(loading => ({ ...loading, subjects: true }));
     const res = await client["subjects"].find({
       query: {
+        "name": query ? {
+          $iLike: `%${query}%`
+        } : undefined,
         $select: ["id", "code", "name"]
       }
     });
@@ -53,17 +57,24 @@ const DialogTambah = ({
     setLoading(loading => ({ ...loading, subjects: false }));
   }, [client]);
 
-  const fetchLecturers = useCallback(async () => {
+  const fetchLecturers = useCallback(async (query) => {
     setLoading(loading => ({ ...loading, lecturers: true }));
     const res = await client["lecturers"].find({
       query: {
+        $limit: 25,
         $select: ["id"],
         $include: [{
           model: "employees",
-          $select: ["id", "nip", "name", "front_degree", "back_degree"]
+          $select: ["id", "nip", "name", "front_degree", "back_degree"],
+          $where: {
+            "name": query ? {
+              $iLike: `%${query}%`
+            } : undefined,
+          }
         }]
       }
     });
+    console.log(res);
     setLecturers(res.data.map(({ id, employee: { nip, name, front_degree, back_degree } }) => ({
       label: `${front_degree || ""}${name}${back_degree || ""}`,
       value: id,
@@ -137,9 +148,12 @@ const DialogTambah = ({
                   onChange={async ({ value }) => {
                     await setFieldValue("subject_id", value, true);
                   }}
+                  onQueryChange={(query) => {
+                    fetchSubjects(query);
+                  }}
                   intent={errors["subject_id"] ? "danger" : "none"}
                   onOpening={async () => {
-                    await fetchSubjects();
+                    await fetchSubjects("");
                   }}
                   options={subjects}
                 />
@@ -158,174 +172,202 @@ const DialogTambah = ({
                   onChange={async ({ value }) => {
                     await setFieldValue("lecturer_id", value, true);
                   }}
+                  onQueryChange={(query) => {
+                    fetchLecturers(query);
+                  }}
                   intent={errors["lecturer_id"] ? "danger" : "none"}
                   onOpening={async () => {
-                    await fetchLecturers();
+                    await fetchLecturers("");
                   }}
                   options={lecturers}
                 />
               </FormGroup>
-              <FieldArray
-                name="hours"
-                render={arr => (
-                  <Box>
-                    {values["hours"].map((v, i) => (
-                      <Flex
-                        key={i}
-                        sx={{
-                          mx: -2,
-                          "> div": { mx: 2, }
-                        }}
-                      >
-                        <Box>
-                          <FormGroup
-                            label="Hari"
-                            labelFor="f-day"
-                            helperText={errors["day"]}
-                            intent={"danger"}
-                            inline={true}
-                          >
-                            <Select
-                              fill={true}
-                              id="f-day"
-                              name={`hours[${i}]["day"]`}
-                              value={v["day"]}
-                              onChange={async ({ value }) => {
-                                await setFieldValue(`hours[${i}]["day"]`, value, true);
+              <Box className={Classes.CARD} sx={{ mx: -2, px: 3 }}>
+                <Box as="h4" sx={{ mb: 2 }}>Waktu</Box>
+                <FieldArray
+                  name="hours"
+                  render={arr => (
+                    <Box>
+                      {values["hours"].map((v, i) => (
+                        <Flex
+                          key={i}
+                          sx={{
+                            mx: -2,
+                            "> div": { mx: 2, }
+                          }}
+                        >
+                          <Box>
+                            <FormGroup
+                              label="Hari"
+                              labelFor="f-day"
+                              helperText={_get(errors, `["hours"][${i}]["day"]`)}
+                              intent={"danger"}
+                              inline={true}
+                            >
+                              <Select
+                                fill={true}
+                                id="f-day"
+                                name={`hours[${i}]["day"]`}
+                                value={v["day"]}
+                                onChange={async ({ value }) => {
+                                  await setFieldValue(`hours[${i}]["day"]`, value, true);
+                                }}
+                                intent={_get(errors, `["hours"][${i}]["day"]`) ? "danger" : "none"}
+                                options={CONSTANTS["DAYS"].map((value, idx) => {
+                                  return { label: value, value: idx }
+                                })}
+                              />
+                            </FormGroup>
+                          </Box>
+                          <Box>
+                            <FormGroup
+                              label="Start"
+                              labelFor="f-start"
+                              helperText={_get(errors, `["hours"][${i}]["start"]`)}
+                              intent={"danger"}
+                              inline={true}
+                            >
+                              <TimePicker
+                                autoFocus={false}
+                                id="f-start"
+                                name={`hours[${i}]["start"]`}
+                                precision="minutes"
+                                value={v["start"]}
+                                onChange={async (value) => {
+                                  await setFieldValue(`hours[${i}]["start"]`, value, true);
+                                }}
+                                intent={_get(errors, `["hours"][${i}]["start"]`) ? "danger" : "none"}
+                              />
+                            </FormGroup>
+                          </Box>
+                          <Box>
+                            <FormGroup
+                              label="End"
+                              labelFor="f-end"
+                              helperText={_get(errors, `["hours"][${i}]["end"]`)}
+                              intent={"danger"}
+                              inline={true}
+                            >
+                              <TimePicker
+                                fill={true}
+                                loading={loading["end"]}
+                                id="f-end"
+                                name={`hours[${i}]["end"]`}
+                                value={v["end"]}
+                                onChange={async (value) => {
+                                  await setFieldValue(`hours[${i}]["end"]`, value, true);
+                                }}
+                                intent={_get(errors, `["hours"][${i}]["end"]`) ? "danger" : "none"}
+                              />
+                            </FormGroup>
+                          </Box>
+                          <Box>
+                            <Button
+                              minimal={true}
+                              icon="cross"
+                              onClick={() => {
+                                arr.remove(i);
                               }}
-                              intent={errors["day"] ? "danger" : "none"}
-                              options={CONSTANTS["DAYS"].map((value, idx) => {
-                                return { label: value, value: idx }
-                              })}
                             />
-                          </FormGroup>
-                        </Box>
-                        <Box>
-                          <FormGroup
-                            label="Start"
-                            labelFor="f-start"
-                            helperText={errors["start"]}
-                            intent={"danger"}
-                            inline={true}
-                          >
-                            <TimePicker
-                              autoFocus={false}
-                              id="f-start"
-                              name={`hours[${i}]["start"]`}
-                              precision="minutes"
-                              value={v["start"]}
-                              onChange={async (value) => {
-                                await setFieldValue(`hours[${i}]["start"]`, value, true);
-                              }}
-                              intent={errors["start"] ? "danger" : "none"}
-                            />
-                          </FormGroup>
-                        </Box>
-                        <Box>
-                          <FormGroup
-                            label="End"
-                            labelFor="f-end"
-                            helperText={errors["end"]}
-                            intent={"danger"}
-                            inline={true}
-                          >
-                            <TimePicker
-                              fill={true}
-                              loading={loading["end"]}
-                              id="f-end"
-                              name={`hours[${i}]["end"]`}
-                              value={v["end"]}
-                              onChange={async (value) => {
-                                await setFieldValue(`hours[${i}]["end"]`, value, true);
-                              }}
-                              intent={errors["end"] ? "danger" : "none"}
-                            />
-                          </FormGroup>
-                        </Box>
-                        <Box>
-                          <Button
-                            minimal={true}
-                            icon="cross"
-                            onClick={() => {
-                              arr.remove(i);
-                            }}
-                          />
-                        </Box>
-                      </Flex>
-                    ))}
-                    <Button
-                      small={true}
-                      minimal={true}
-                      text="Tambah waktu"
-                      onClick={() => arr.push({
-                        day: null, start: new Date(), end: new Date()
-                      })}
-                    />
-                  </Box>
-                )}
-              />
+                          </Box>
+                        </Flex>
+                      ))}
+                      <Button
+                        small={true}
+                        outlined={true}
+                        text="Tambah waktu"
+                        onClick={() => arr.push({
+                          day: null, start: new Date(), end: new Date()
+                        })}
+                      />
+                    </Box>
+                  )}
+                />
+              </Box>
               <h4>Bobot nilai</h4>
+              <Flex sx={{ mx: -2, flexWrap: "wrap" }}>
+                <Box sx={{ width: "50%", px: 2 }}>
+                  <FormGroup
+                    label="Kehadiran"
+                    labelFor="f-presence_weight"
+                    helperText={errors["presence_weight"]}
+                    intent={"danger"}
+                  >
+                    <InputGroup
+                      id="f-presence_weight"
+                      name="presence_weight"
+                      value={values["presence_weight"]}
+                      onChange={handleChange}
+                      intent={errors["presence_weight"] ? "danger" : "none"}
+                      rightElement={<Tag minimal={true}>%</Tag>}
+                    />
+                  </FormGroup>
+                </Box>
+                <Box sx={{ width: "50%", px: 2 }}>
+                  <FormGroup
+                    label="Tugas"
+                    labelFor="f-task_weight"
+                    helperText={errors["task_weight"]}
+                    intent={"danger"}
+                  >
+                    <InputGroup
+                      id="f-task_weight"
+                      name="task_weight"
+                      value={values["task_weight"]}
+                      onChange={handleChange}
+                      intent={errors["task_weight"] ? "danger" : "none"}
+                      rightElement={<Tag minimal={true}>%</Tag>}
+                    />
+                  </FormGroup>
+                </Box>
+                <Box sx={{ width: "50%", px: 2 }}>
+                  <FormGroup
+                    label="Mid Semester"
+                    labelFor="f-mid_test_weight"
+                    helperText={errors["mid_test_weight"]}
+                    intent={"danger"}
+                  >
+                    <InputGroup
+                      id="f-mid_test_weight"
+                      name="mid_test_weight"
+                      value={values["mid_test_weight"]}
+                      onChange={handleChange}
+                      intent={errors["mid_test_weight"] ? "danger" : "none"}
+                      rightElement={<Tag minimal={true}>%</Tag>}
+                    />
+                  </FormGroup>
+                </Box>
+                <Box sx={{ width: "50%", px: 2 }}>
+                  <FormGroup
+                    label="Akhir Semester"
+                    labelFor="f-final_test_weight"
+                    helperText={errors["final_test_weight"]}
+                    intent={"danger"}
+                  >
+                    <InputGroup
+                      id="f-final_test_weight"
+                      name="final_test_weight"
+                      value={values["final_test_weight"]}
+                      onChange={handleChange}
+                      intent={errors["final_test_weight"] ? "danger" : "none"}
+                      rightElement={<Tag minimal={true}>%</Tag>}
+                    />
+                  </FormGroup>
+                </Box>
+              </Flex>
               <FormGroup
-                label="Kehadiran"
-                labelFor="f-presence_weight"
-                helperText={errors["presence_weight"]}
+                label="Total"
+                labelFor="f-total_weight"
+                helperText={errors["total_weight"]}
                 intent={"danger"}
-                inline={true}
               >
                 <InputGroup
-                  id="f-presence_weight"
-                  name="presence_weight"
-                  value={values["presence_weight"]}
+                  readOnly={true}
+                  id="f-total_weight"
+                  name="total_weight"
+                  value={(Number(values["final_test_weight"]) + Number(values["mid_test_weight"]) + Number(values["task_weight"]) + Number(values["presence_weight"]))}
                   onChange={handleChange}
-                  intent={errors["presence_weight"] ? "danger" : "none"}
-                  rightElement={<Tag minimal={true}>%</Tag>}
-                />
-              </FormGroup>
-              <FormGroup
-                label="Tugas"
-                labelFor="f-task_weight"
-                helperText={errors["task_weight"]}
-                intent={"danger"}
-                inline={true}
-              >
-                <InputGroup
-                  id="f-task_weight"
-                  name="task_weight"
-                  value={values["task_weight"]}
-                  onChange={handleChange}
-                  intent={errors["task_weight"] ? "danger" : "none"}
-                  rightElement={<Tag minimal={true}>%</Tag>}
-                />
-              </FormGroup>
-              <FormGroup
-                label="Mid Semester"
-                labelFor="f-mid_test_weight"
-                helperText={errors["mid_test_weight"]}
-                intent={"danger"}
-                inline={true}
-              >
-                <InputGroup
-                  id="f-mid_test_weight"
-                  name="mid_test_weight"
-                  value={values["mid_test_weight"]}
-                  onChange={handleChange}
-                  intent={errors["mid_test_weight"] ? "danger" : "none"}
-                  rightElement={<Tag minimal={true}>%</Tag>}
-                />
-              </FormGroup>
-              <FormGroup
-                label="Akhir Semester"
-                labelFor="f-final_test_weight"
-                helperText={errors["final_test_weight"]}
-                intent={"danger"}
-                inline={true}
-              >
-                <InputGroup
-                  id="f-final_test_weight"
-                  name="final_test_weight"
-                  value={values["final_test_weight"]}
-                  onChange={handleChange}
-                  intent={errors["final_test_weight"] ? "danger" : "none"}
+                  intent={errors["total_weight"] ? "danger" : "none"}
                   rightElement={<Tag minimal={true}>%</Tag>}
                 />
               </FormGroup>
